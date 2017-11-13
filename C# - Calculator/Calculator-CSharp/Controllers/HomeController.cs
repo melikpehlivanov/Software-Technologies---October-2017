c7bbe3fc6f8a7e34659078abd98cccf7dcf81a75
melik <melikpehlivanov@gmail.com>
2017-11-13 17:13:09 +0200

    C# - Calculator

---
+++ C# - Calculator/Calculator-CSharp/Controllers/HomeController.cs
@@ -0,0 +1,47 @@
+﻿using System.Web.Mvc;
+
+namespace Calculator_CSharp.Controllers
+{
+    using System;
+    using Models;
+
+    public class HomeController : Controller
+    {
+
+        public ActionResult Index(Calculator calculator)
+        {
+            return View(calculator);
+        }
+
+        private decimal CalculateResult(Calculator calculator)
+        {
+            decimal result = 0m;
+
+            switch (calculator.Operator)
+            {
+                case "+":
+                    result = calculator.LeftOperand + calculator.RightOperand;
+                    break;
+                case "*":
+                    result = calculator.LeftOperand * calculator.RightOperand;
+                    break;
+                case "/":
+                    result = calculator.LeftOperand / calculator.RightOperand;
+                    break;
+                case "-":
+                    result = calculator.LeftOperand - calculator.RightOperand;
+                    break;
+            }
+
+            return result;
+        }
+
+        [HttpPost]
+        public ActionResult Calculate(Calculator calculator)
+        {
+            calculator.Result = CalculateResult(calculator);
+
+            return RedirectToAction("Index", calculator);
+        }
+    }
+}
