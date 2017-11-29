194c8bd212c6741c1fafc5606844e7bcf289fff4
Melik <melikpehlivanov@gmail.com>
2017-11-29 14:14:22 +0200

    Update P02BooleanVariable.java

--- Java Basics - Exercises/src/P02BooleanVariable.java
+++ Java Basics - Exercises/src/P02BooleanVariable.java
@@ -1,15 +1,13 @@
 import java.util.Scanner;
 
 public class P02BooleanVariable {
-    public static void main() {
+    public static void main(String[] args) {
+
         Scanner scan = new Scanner(System.in);
         Boolean boolResult = Boolean.parseBoolean(scan.nextLine());
-        if (boolResult){
+        if (boolResult)
             System.out.println("Yes");
-        }
-        else{
+        else
             System.out.println("No");
-        }
     }
-
 }
